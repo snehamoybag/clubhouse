f8f203524960983c1414f4b682679ffec4066610
snehamoybag <102692389+snehamoybag@users.noreply.github.com>
2025-01-07 21:01:42 +0530

    refractor: rename isAuthenticated middleware to handleNotAuthenticated

--- middlewares/handleNotAuthenticated.js
+++ middlewares/handleNotAuthenticated.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   if (!req.isAuthenticated()) {
     next(
       new CustomAccessDeniedError(
-        "You do not have permission to access this page.",
+        "You do not have permission to access this resource.",
       ),
     );
   }
